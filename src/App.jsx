d8dc838d8496331e327a64ec3a6d5b75d414cb64
Mahmoud Ragaei <mahmoud_egypt_eng@hotmail.com>
2024-01-05 17:04:55 +0200

    apply survey

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,158 @@
+import 'survey-core/defaultV2.min.css';
+import { Model } from 'survey-core';
+import { surveyJson } from './Components/data.js'
+import { Survey } from 'survey-react-ui';
+import { useCallback } from 'react';
+import MyChart from './Components/MyChart.tsx';
 import { useState } from 'react'
-import { CORE_CONCEPTS } from './data.js'
-import componentsImg from './assets/components.png'
-import Header from './Components/Header.jsx'
-import CoreConcept from './Components/CoreConcept.jsx'
-import TabButton from './Components/TabButton.jsx'
-import { EXAMPLES } from './data.js'
-const surveyJson = {
-  title: "American History",
-  showProgressBar: "bottom",
-  firstPageIsStarted: true,
-  startSurveyText: "Start Quiz",
-  pages: [{
-      elements: [{
-          type: "html",
-          html: "You are about to start a quiz on American history. <br>You will have 10 seconds for every question and 25 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
-      }, {
-          type: "text",
-          name: "username",
-          titleLocation: "hidden",
-          isRequired: true
-      }]
-  }, {
-      elements: [{
-          type: "radiogroup",
-          name: "civilwar",
-          title: "غاتساا ",
-          choices: [
-              "1796-1803", "1810-1814", "1861-1865", "1939-1945"
-          ],
-          correctAnswer: "1861-1865"
-      }]
-  }, {
-      elements: [{
-          type: "radiogroup",
-          name: "libertyordeath",
-          title: "Whose quote is this: \"Give me liberty, or give me death\"?",
-          choicesOrder: "random",
-          choices: [
-              "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-          ],
-          correctAnswer: "Patrick Henry"
-      }]
-  }, {
-      elements: [{
-          type: "radiogroup",
-          name: "magnacarta",
-          title: "What is Magna Carta?",
-          choicesOrder: "random",
-          choices: [
-              "The foundation of the British parliamentary system",
-              "The Great Seal of the monarchs of England",
-              "The French Declaration of the Rights of Man",
-              "The charter signed by the Pilgrims on the Mayflower"
-          ],
-          correctAnswer: "The foundation of the British parliamentary system"
-      }]
-  }],
-  completedHtml: "<h4>You got <b>{correctAnswers}</b> out of <b>{questionCount}</b> correct answers.</h4>",
-  completedHtmlOnCondition: [{
-      expression: "{correctAnswers} == 0",
-      html: "<h4>Unfortunately, none of your answers are correct. Please try again.</h4>"
-  }, {
-      expression: "{correctAnswers} == {questionCount}",
-      html: "<h4>Congratulations! You answered all the questions correctly!</h4>"
-  }]
-};
+
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-console.log(surveyJson);
-  function handleSelect(selectedButton) {
-    //console.log(selectedButton)
-    setSelectedTopic(selectedButton);
-  }
 
-  return (
-    <div>
-      <Header />
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept{...conceptItem} />)}
-          </ul>
-        </section>
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic == 'components'}
-              onSelect={() => handleSelect('components')}>components</TabButton>
-            <TabButton isSelected={selectedTopic == 'jsx'}
-              onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic == 'props'}
-              onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic == 'state'}
-              onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          {!selectedTopic && <p>Please select a topic</p>}
-          {selectedTopic && <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>}
-        </section>
-      </main>
-    </div>
-  );
+  const [data, updateDate] = useState();
+
+  const survey = new Model(surveyJson);
+
+  const alertResults = useCallback((sender) => {
+    console.log(sender.data);
+    calcScores(sender.data);
+  }, []);
+
+  const calcScores = useCallback((sender) => {
+    console.log(sender);
+    calcScore(sender, updateDate);
+  }, []);
+
+  survey.onComplete.add(alertResults);
+  return (<section>
+    {!data && <Survey model={survey} />}
+    {data && <MyChart data={data} />}
+  </section>);
+  //return <MyChart data ={data}/>
+}
+function calcSelectScore(select) {
+  if (select === 'آه')
+    return 1;
+  if (select === 'بتحصل')
+    return 0.5;
+  return 0;
+}
+function calcScore(result, updateDate) {
+  let score1 = 0;
+  score1 = score1 + calcSelectScore(result.question.q6);
+  score1 = score1 + calcSelectScore(result.question.q13);
+  score1 = score1 + calcSelectScore(result.question.q20);
+  score1 = score1 + calcSelectScore(result.question.q27);
+  score1 = score1 + calcSelectScore(result.question.q34);
+  score1 = score1 + calcSelectScore(result.question.q36);
+
+  let score2 = 0;
+  score2 = score2 + calcSelectScore(result.question.q4);
+  score2 = score2 + calcSelectScore(result.question.q11);
+  score2 = score2 + calcSelectScore(result.question.q18);
+  score2 = score2 + calcSelectScore(result.question.q25);
+  score2 = score2 + calcSelectScore(result.question.q32);
+  score2 = score2 + calcSelectScore(result.question.q41);
+
+  let score3 = 0;
+  score3 = score3 + calcSelectScore(result.question.q7);
+  score3 = score3 + calcSelectScore(result.question.q14);
+  score3 = score3 + calcSelectScore(result.question.q21);
+  score3 = score3 + calcSelectScore(result.question.q28);
+  score3 = score3 + calcSelectScore(result.question.q35);
+  score3 = score3 + calcSelectScore(result.question.q37);
+
+  let score4 = 0;
+  score4 = score4 + calcSelectScore(result.question.q5);
+  score4 = score4 + calcSelectScore(result.question.q12);
+  score4 = score4 + calcSelectScore(result.question.q19);
+  score4 = score4 + calcSelectScore(result.question.q26);
+  score4 = score4 + calcSelectScore(result.question.q33);
+  score4 = score4 + calcSelectScore(result.question.q42);
+
+  let score5 = 0;
+  score5 = score5 + calcSelectScore(result.question.q1);
+  score5 = score5 + calcSelectScore(result.question.q8);
+  score5 = score5 + calcSelectScore(result.question.q15);
+  score5 = score5 + calcSelectScore(result.question.q22);
+  score5 = score5 + calcSelectScore(result.question.q29);
+  score5 = score5 + calcSelectScore(result.question.q38);
+
+  let score6 = 0;
+  score6 = score6 + calcSelectScore(result.question.q2);
+  score6 = score6 + calcSelectScore(result.question.q9);
+  score6 = score6 + calcSelectScore(result.question.q16);
+  score6 = score6 + calcSelectScore(result.question.q23);
+  score6 = score6 + calcSelectScore(result.question.q30);
+  score6 = score6 + calcSelectScore(result.question.q39);
+
+  let score7 = 0;
+  score7 = score7 + calcSelectScore(result.question.q3);
+  score7 = score7 + calcSelectScore(result.question.q10);
+  score7 = score7 + calcSelectScore(result.question.q17);
+  score7 = score7 + calcSelectScore(result.question.q24);
+  score7 = score7 + calcSelectScore(result.question.q31);
+  score7 = score7 + calcSelectScore(result.question.q40);
+  
+
+  updateDate([
+    {
+      data: [
+        {
+          Category: "السخط",
+          Score: score1,
+        }
+      ]
+    },
+    {
+      data: [
+        {
+          Category: "الضعف والهوان",
+          Score: score2,
+        }
+      ]
+    },
+    {
+      data: [
+        {
+          Category: "الفوات",
+          Score: score3,
+        }
+      ]
+    },
+    {
+      data: [
+        {
+          Category: "الملل والكسل",
+          Score: score4,
+        }
+      ]
+    },
+    {
+      data: [
+        {
+          Category: "الوحشة",
+          Score: score5,
+        }
+      ]
+    },
+    {
+      data: [
+        {
+          Category: "فقد الأمل",
+          Score: score6,
+        }
+      ]
+    },
+    {
+      data: [
+        {
+          Category: "قصر النظر",
+          Score: score7,
+        }
+      ]
+    }
+  ]);
 }
 
 export default App;
---
+++ src/Components/MyChart.tsx
@@ -0,0 +1,58 @@
+import ResizableBox from "./ResizableBox";
+import React from "react";
+import { AxisLinearOptions, AxisOptions, Chart } from "react-charts";
+
+
+type CategoryScore = {
+    Category: string,
+    Score: number,
+}
+type Series = {
+    label: string,
+    data: CategoryScore[]
+}
+
+export default function MyChart({data}) {
+    const primaryAxis = React.useMemo<
+        AxisOptions<CategoryScore>
+    >(
+        () => ({
+            getValue: (datum) => datum.Category,
+        }),
+        []
+    );
+
+    const secondaryAxes = React.useMemo<
+    AxisLinearOptions<CategoryScore>[]
+    >(
+        () => [
+            {
+                getValue: (datum) => datum.Score,
+                minDomainLength: 6,
+                min: 0,
+                max: 6,
+                hardMin: 0
+            },
+        ],
+        []
+    );
+
+    const padding = 0
+    
+    return (
+        <>
+            <br />
+            <br />
+            <ResizableBox>
+                <Chart
+                    options={{
+                        data,
+                        primaryAxis,
+                        secondaryAxes,
+                        padding,
+                    }}
+                />
+            </ResizableBox>
+        </>
+    );
+}
